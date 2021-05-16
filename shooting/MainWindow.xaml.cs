7a13fdfbcb9d8f50df896e10e871c45e15867193
veta1428 <lizavetka1428@gmail.com>
2021-05-16 11:26:03 +0300

    Final

---
+++ shooting/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using System.Windows.Shapes;
+
+namespace shooting
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {       
+        public MainWindow()
+        {
+            InitializeComponent();     
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {          
+                showmistake.Content = " ";
+        }
+
+        private void CheckBounds(int radius)
+        {
+            if (radius > 445 || radius < 20)
+            {
+                throw new Exception();
+            }
+        }
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+            Tuple<int, bool> info = TryToParse();
+            InfoOrNewWindow(info);              
+        }
+
+        private void InfoOrNewWindow(Tuple<int, bool> info)
+        {
+            if (info.Item2 == false)
+            {
+                showmistake.Content = "Wrong input!";
+            }
+            else
+            {
+                Shooting SecondForm = new Shooting(info.Item1);                
+                SecondForm.Show();
+                MainForm.Close();
+            }
+        }
+
+        private Tuple<int, bool> TryToParse()
+        {
+            int radius = 0;
+            try
+            {
+                radius = Convert.ToInt32(textbox.Text);
+                CheckBounds(radius);    
+            }
+            catch
+            {
+                return Tuple.Create(0, false);
+            }
+
+            return Tuple.Create(radius, true);
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Tuple<int, bool> info = TryToParse();
+                InfoOrNewWindow(info);
+            }
+        }
+    }
+}
---
+++ shooting/Shooting.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace shooting
+{
+    /// <summary>
+    /// Логика взаимодействия для Shooting.xaml
+    /// </summary>
+    public partial class Shooting : Window
+    {
+        private int Radius;
+
+        private int succeededhits = 0;
+
+        private int allHits = 0;
+
+        public Shooting(int radius)
+        {            
+            InitializeComponent();
+            Radius = radius;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Radius > 205)
+            {
+                this.Height = 2 * Radius + 40;
+                this.Width = 1.7 * this.Height;                
+            }
+
+            this.MinHeight = this.Height;
+            this.MinWidth = this.MinHeight;
+
+            gridforshooting.Height = 2 * this.Height;
+            gridforshooting.Width = 2 * this.Width;           
+
+            targety.Radius = Radius;
+            targety.Centre = new Point(gridforshooting.Width / 2, gridforshooting.Height / 2);        
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            allhitsnumber.Content = ++allHits;
+
+            Point coord = e.GetPosition(gridforshooting);
+            Ellipse circle = new Ellipse();
+            circle.Height = 5;
+            circle.Width = 5;
+            circle.Fill = Brushes.Black;
+            circle.VerticalAlignment = VerticalAlignment.Top;
+            circle.HorizontalAlignment = HorizontalAlignment.Left;
+            circle.Margin = new Thickness(coord.X, coord.Y, 0, 0);
+            gridforshooting.Children.Add(circle);
+        }
+
+        private void targety_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            hitsnumber.Content = ++succeededhits; 
+        }
+    }
+}
